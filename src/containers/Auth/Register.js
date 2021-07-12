6b69c81bda13c61aefaa1c7c37c3fed07f15009c
minjikim-gym <mj.kim@gymmon.work>
2021-07-12 15:44:46 +0900

    first commit

---
+++ src/containers/Auth/Register.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink, AuthError } from '../../components/Auth';
+import {isEamil, isLength, isAlphanumeric} from 'validator';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as authActions from '../../redux/modules/auth';
+import debounce from 'lodash/debounce';
+import * as userActions from '../../redux/modules/user';
+import storage from '../../lib/storage';
+
+class Register extends Component {
+
+    setError = (message) => {
+        const { AuthActions } = this.props;
+        AuthActions.setError({
+            form: 'register',
+            message
+        });
+    }
+
+    validate = {
+        email: (value) => {
+            if(!isEamil(value)) {
+                this.setError('잘못된 이메일 형식 입니다.');
+                return false;
+            }
+            return true;
+        },
+        username: (value) => {
+            if(!isAlphanumeric(value) || !isLength(value, { min:4, max:15 })) {
+                this.setError('아이디는 4~15 글자의 알파벳 혹은 숫자로 이뤄져야 합니다.');
+                return false;
+            }
+            return true;
+        },
+        password: (value) => {
+            if(!isLength(value, { min: 6 })) {
+                this.setError('비밀번호를 6자 이상 입력하세요.');
+                return false;
+            }
+            this.setError(null);
+            return true;
+        },
+        passwordConfirm: (value) => {
+            if(this.props.form.get('password') !== value) {
+                this.setError('비밀번호확인이 일치하지 않습니다.');
+                return false;
+            }
+            this.setError(null);
+            return true;
+        }
+    }
+
+    checkEmailExists = debounce(async (email) => {
+        const { AuthActions } = this.props;
+        try {
+            await AuthActions.checkEmailExists(email);
+            if(this.props.exists.get('email')) {
+                this.setError('이미 존재하는 이메일입니다.');
+            } else {
+                this.setError(null);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }, 300)
+
+    checkUsernameExists = debounce(async (username) => {
+        const { AuthActions } = this.props;
+        try {
+            await AuthActions.checkUsernameExists(username);
+            if(this.props.exists.get('username')) {
+                this.setError('이미 존재하는 아이디입니다.');
+            } else {
+                this.setError(null);
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }, 300)
+
+    handleChange = (e) => {
+        const { AuthActions } = this.props;
+        const { name, value } = e.target;
+
+        AuthActions.changeInput({
+            name,
+            value,
+            form: 'register'
+        });
+
+        // 검증작업 진행
+        const validation = this.validate[name](value);
+        if(name.indexOf('password') > -1 || !validation) return; // 비밀번호 검증이거나, 검증 실패하면 여기서 마침
+
+        // TODO: 이메일, 아이디 중복 확인
+        const check = name === 'email' ? this.checkEmailExists : this.checkUsernameExists;
+        check(value);
+    }
+
+    handleLocalRegister = async () => {
+        const { form, AuthActions,UserActions, error, history } = this.props;
+        const { email, username, password, passwordConfirm } = form.toJS();
+
+        const { validate } = this;
+
+        if(error) return;
+        if(!validate['email'](email)
+            || !validate['username'](username)
+            || !validate['password'](password)
+            || !validate['passwordConfirm'](passwordConfirm)){
+            // 하나라도 실패하면 진행 X
+            return;
+        }
+        try {
+            await AuthActions.localRegister({
+                email, username, password
+            });
+            const loggedInfo = this.props.result.toJS();
+            storage.set('loggedInfo');
+            UserActions.setLoggedInfo(loggedInfo);
+            UserActions.setValidated(loggedInfo);
+            // 로그인 정보 저장 (로컬스퇴리지/스토어)
+            history.push('/');
+        } catch(e) {
+            // 에러 처리하기
+            if(e.response.status === 409) {
+                const { key } = e.response.data;
+                const message = key === 'email' ? '이미 존재하는 이메일입니다.' : '이미 존재하는 아이디입니다.';
+                return this.setError(message);
+            }
+            this.setError('알 수 없는 에러가 발생했습니다.')
+        }
+    }
+
+    render() {
+
+        const { error } = this.props;
+        const { email, username, password, passwordConfirm } = this.props.form.toJS();
+        const { handleChange } = this;
+
+        return (
+            <AuthContent title="회원가입">
+                <InputWithLabel label="이메일" name="email" placeholder="이메일"/>
+                <InputWithLabel label="아이디" name="username" placeholder="아이디"/>
+                <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password"/>
+                <InputWithLabel label="비밀번호 확인" name="passwordConfirm" placeholder="비밀번호 확인" type="password" value={passwordConfirm} onChange={handleChange}/>
+                {
+                    error && <AuthError>{error}</AuthError>
+                }
+                <AuthButton onClick={this.handleLocalRegister}>회원가입</AuthButton>                
+                <RightAlignedLink to="/auth/login">로그인</RightAlignedLink>
+            </AuthContent>
+        );
+    }
+}
+
+export default connect(
+    (state) => ({
+        form: state.auth.getIn(['register', 'form']),
+        error: state.auth.getIn(['register', 'error']),
+        exists: state.auth.getIn(['register', 'exists']),
+        result: state.auth.get('result')
+    }),
+    (dispatch) => ({
+        AuthActions: bindActionCreators(authActions, dispatch),
+        UserActions: bindActionCreators(userActions, dispatch)
+    })
+)(Register);
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './Root';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import configureStore from './redux/configureStore';
+
+const store = configureStore();
+
+ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
